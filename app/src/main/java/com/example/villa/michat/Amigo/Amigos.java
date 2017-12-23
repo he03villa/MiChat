857e67f31c1884367ab907c60c72d6f0f973a4d0
he03villa <he03villa@gmail.com>
2017-12-22 22:00:26 -0500

    subiendo

--- app/src/main/java/com/example/villa/michat/Amigo/Amigos.java
+++ app/src/main/java/com/example/villa/michat/Amigo/Amigos.java
@@ -1,16 +1,28 @@
 package com.example.villa.michat.Amigo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.example.villa.michat.Mesajes.MensajeDeTexto;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.villa.michat.Login;
+import com.example.villa.michat.Mesajes.Mensajeria;
+import com.example.villa.michat.Preferences;
 import com.example.villa.michat.R;
+import com.example.villa.michat.VolleyRP;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.List;
 
 /**
@@ -23,22 +35,48 @@ public class Amigos extends AppCompatActivity{
     private List<AmigosAtributos> atributolista;
     private AmigosAdapter adapter;
 
+    private RequestQueue mRequest;
+    private VolleyRP volleyRP;
+
+    private static final String URL = "http://he03villa.000webhostapp.com/chat/Controlador/Amigos/Lista.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amigo);
         setTitle("Amigos");
 
+        volleyRP = VolleyRP.getInstance(this);
+        mRequest = volleyRP.getRequestQueue();
+
         rv = (RecyclerView)findViewById(R.id.rvAmigos);
         LinearLayoutManager lm = new LinearLayoutManager(this);
         rv.setLayoutManager(lm);
 
         adapter = new AmigosAdapter(atributolista,this);
-        rv.setAdapter(adapter);
-        for (int i=0;i<10;i++) agrgarAmigo(R.drawable.ic_account_circle,"usuario "+i,"mensaje "+i,"00:00");
+        //rv.setAdapter(adapter);
+        SolicitudJSON();
     }
 
-    public void agrgarAmigo(int fotoDePerfil,String nombre,String mensaje,String hora){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_amigos,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.CerrarSesionMenu){
+            Preferences.saveBoolean(Amigos.this,false,Preferences.STRING_ESTADO);
+            Intent intent = new Intent(Amigos.this,Login.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void agrgarAmigo(int fotoDePerfil, String nombre, String mensaje, String hora){
         AmigosAtributos atributos = new AmigosAtributos();
         atributos.setFotoDePerfil(fotoDePerfil);
         atributos.setMensaje(mensaje);
@@ -46,5 +84,30 @@ public class Amigos extends AppCompatActivity{
         atributos.setHora(hora);
         atributolista.add(atributos);
         adapter.notifyDataSetChanged();
+    }
+
+    public void SolicitudJSON(){
+        JsonObjectRequest solicitud = new JsonObjectRequest(URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String datos = response.getString("resultado");
+                    JSONArray array = new JSONArray(datos);
+                    for(int i = 0; i<array.length();i++){
+                        JSONObject object = array.getJSONObject(i);
+                        Toast.makeText(Amigos.this,"El usuario "+i+" es "+object.getString("id"),Toast.LENGTH_LONG).show();
+                        agrgarAmigo(R.drawable.ic_account_circle,object.getString("nombre"),"mensaje "+i,"00:00");
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(Amigos.this,"Error en la descompresion del json",Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Amigos.this,"Error en la conexion",Toast.LENGTH_LONG).show();
+            }
+        });
+        VolleyRP.addToQueue(solicitud,mRequest,this,volleyRP);
     }
 }
--- app/src/main/java/com/example/villa/michat/Mesajes/Mensajeria.java
+++ app/src/main/java/com/example/villa/michat/Mesajes/Mensajeria.java
@@ -55,7 +55,6 @@ public class Mensajeria extends AppCompatActivity{
     private EditText txtreceptor;
     private List<MensajeDeTexto> mensajeDeTextos;
     private MensajeAdapter adapter;
-    private Button cerrar;
 
     private RequestQueue mRequest;
     private VolleyRP volleyRP;
@@ -82,7 +81,6 @@ public class Mensajeria extends AppCompatActivity{
         btnenviar = (Button) findViewById(R.id.btnenviar);
         txtmensaje = (EditText) findViewById(R.id.txtmensaje);
         txtreceptor = (EditText) findViewById(R.id.txtreceptor);
-        cerrar = (Button) findViewById(R.id.btncerrar);
 
         rv = (RecyclerView) findViewById(R.id.rvMensaje);
         LinearLayoutManager lm = new LinearLayoutManager(this);
@@ -124,16 +122,6 @@ public class Mensajeria extends AppCompatActivity{
                 CreateMendaje(mensaje,hora,2);
             }
         };
-
-        cerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Preferences.saveBoolean(Mensajeria.this,false,Preferences.STRING_ESTADO);
-                Intent intent = new Intent(Mensajeria.this,Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 
     //" " no va a enviar
@@ -187,15 +175,6 @@ public class Mensajeria extends AppCompatActivity{
         LocalBroadcastManager.getInstance(this).unregisterReceiver(bR);
     }
 
-    /**
-     * Dispatch onResume() to fragments.  Note that for better inter-operation
-     * with older versions of the platform, at the point of this call the
-     * fragments attached to the activity are <em>not</em> resumed.  This means
-     * that in some cases the previous state may still be saved, not allowing
-     * fragment transactions that modify the state.  To correctly interact
-     * with fragments in their proper state, you should instead override
-     * {@link #onResumeFragments()}.
-     */
     @Override
     protected void onResume() {
         super.onResume();
